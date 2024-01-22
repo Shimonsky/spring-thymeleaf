d3ecfb2da66d263f51b13f9ba4f93389c9441607
Shimon S <shimonslit@gmail.com>
2024-01-22 15:18:24 +0100

    firstLeaf

---
+++ src/main/java/com/example/thymleaf/controller/DoctorController.java
@@ -0,0 +1,17 @@
+package com.example.thymleaf.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/")
+    public String doctor(@RequestParam(required=false, defaultValue="0") int number,
+                         @RequestParam(required=false, defaultValue="John Smith") String name, Model model) {
+        model.addAttribute("doctorname", name);
+        model.addAttribute("doctornumber", number);
+        return "doctor";
+    }
+}
